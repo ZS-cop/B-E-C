668ff7ca4e8f0db2eff1d8663fc865bdf2caef95
Zodiac Software <86096723+ZS-cop@users.noreply.github.com>
2022-05-15 23:01:46 +0530

    Mood

--- script.js
+++ script.js
@@ -1,54 +1,95 @@
-// Importing database
-let PouchDB = require('pouchdb');
-let db = new PouchDB('Accounts_Database');
+// // Importing database
+// let couchdb = require('node-couchdb');
+// const couch = new couchdb({
+//   host: 'couchdb.external.service',
+//   protocol: 'http',
+//   port: 5984
+// });
+// couch.createDatabase("user_accounts").then(() => (res , err) => {
+//   if(err){
+//     console.log(err)
+//   }
+//   else{
+//     window.alert("h")
+//   }
+// });
 
 // Encryption
-let encryption = password => {
+let encryption = (password) => {
   let passworde = CryptoJS.AES.encrypt(password, "");
   return passworde.toString();
 };
 
 console.log(encryption("Hello world"));
 // Adding the account
-document.getElementById("btn_join").addEventListener("click", () => {
-  if (document.getElementById("password_join").value == document.getElementById("password_joinr").value) {
-    if (document.getElementById("btn_join").style.opacity == 1) {
-      db.put({
-        _id: encryption(document.getElementById("username_join").value),
-        password: encryption(document.getElementById("password_join").value),
-        email: encryption(document.getElementById("email_join").value),
-        name: encryption(document.getElementById("name_join").value)
-      }).then(function (response) {
-        console.log("Done")
-        window.alert("Done")
-      }).catch(function (err) {
-        console.log(err);
-        window.alert("Error email us the error on devyanshnarayanthegreat@gmail.com")
-      });
-    }
-    else{window.alert("Fill the information")}
-  }
-  else{window.alert("Please check your retyped password/information not filled")}
-});
+// document.getElementById("btn_join").addEventListener("click", () => {
+//   if (document.getElementById("password_join").value == document.getElementById("password_joinr").value) {
+//     if (document.getElementById("btn_join").style.opacity == 1) {
+//       // db.put({
+//       //   _id: encryption(document.getElementById("username_join").value),
+//       //   password: encryption(document.getElementById("password_join").value),
+//       //   email: encryption(document.getElementById("email_join").value),
+//       //   name: encryption(document.getElementById("name_join").value)
+//       // }).then(function (response) {
+//       //   console.log("Done")
+//       //   window.alert("Done")
+//       // }).catch(function (err) {
+//       //   console.log(err);
+//       //   window.alert("Error email us the error on devyanshnarayanthegreat@gmail.com")
+//       // });
+//       // db.changes().on('change', function() {
+//       //   console.log('Ch-Ch-Changes');
+//       // });
 
-let input_list = [document.getElementById("username_login"), document.getElementById("password_login")];
+//       // db.replicate.to('http://127.0.0.1:5984/_utils/#database/accounts_user/_all_docs');
+//       db.saveDoc({
+//         _id: encryption(document.getElementById("username_join").value),
+//         password: encryption(document.getElementById("password_join").value),
+//         email: encryption(document.getElementById("email_join").value),
+//         name: encryption(document.getElementById("name_join").value)
+//       }).then(function () {
+//         console.log('document saved!');
 
-input_list.forEach(elements => {
+//         db.openDoc('helloCouch').then(function (doc) {
+//           console.log('retrieved document!');
+//           console.log(JSON.stringify(doc));
+//         });
+//       });
+//     }
+//     else { window.alert("Fill the information") }
+//   }
+//   else { window.alert("Please check your retyped password/information not filled") }
+// });
+
+let input_list = [
+  document.getElementById("username_login"),
+  document.getElementById("password_login"),
+];
+
+input_list.forEach((elements) => {
   elements.addEventListener("input", () => {
-    if (document.getElementById("username_login").value != "" && document.getElementById("password_login").value != "") {
+    if (
+      document.getElementById("username_login").value != "" &&
+      document.getElementById("password_login").value != ""
+    ) {
       document.getElementById("btn_login").style.opacity = "1";
-    }
-    else {
+    } else {
       document.getElementById("btn_login").style.opacity = "0.5";
     }
-  })
+  });
+});
+
+document.addEventListener("keypress", (e) => {
+  if (e.shiftKey && e.key === 'D') {
+    document.querySelector("html").classList.toggle("dark");
+  }
 });
 
 document.getElementById("join_log").addEventListener("click", () => {
   document.querySelector("title").innerHTML = "BO E COMMERCE - JOIN US";
   document.getElementById("login_box").setAttribute("class", "none");
   document.getElementById("join_box").setAttribute("class", "flex center cols white md-radius shadow");
-})
+});
 
 document.getElementById("log_join").addEventListener("click", () => {
   document.querySelector("title").innerHTML = "BO E COMMERCE - LOGIN";
@@ -56,36 +97,39 @@ document.getElementById("log_join").addEventListener("click", () => {
   document.getElementById("login_box").setAttribute("class", "flex center cols white md-radius shadow");
 });
 
-let input_list_join = [document.getElementById("username_join"), document.getElementById("name_join"), document.getElementById("email_join"), document.getElementById("password_join"), document.getElementById("password_joinr")]
+let input_list_join = [
+  document.getElementById("username_join"),
+  document.getElementById("name_join"),
+  document.getElementById("email_join"),
+  document.getElementById("password_join"),
+  document.getElementById("password_joinr"),
+];
 
-input_list_join.forEach(elements => {
+input_list_join.forEach((elements) => {
   elements.addEventListener("input", () => {
     let r = -3;
-    input_list_join.forEach(elements => {
+    input_list_join.forEach((elements) => {
       if (elements.value != "") {
         r += 1;
-      }
-      else {
+      } else {
         r -= 1;
       }
     });
     if (r > 0) {
       document.getElementById("btn_join").style.opacity = "1";
-    }
-    else {
+    } else {
       document.getElementById("btn_join").style.opacity = "0.5";
     }
-  })
+  });
 });
 
 let join_btn = (info) => {
   if (info.value != "") {
     document.getElementById("btn_join").style.opacity = "1";
-  }
-  else {
+  } else {
     document.getElementById("btn_join").style.opacity = "0.5";
   }
-}
+};
 
 let psj = false;
 
@@ -93,28 +137,30 @@ document.getElementById("password_see_join").addEventListener("click", () => {
   if (psj == false) {
     document.getElementById("password_join").setAttribute("type", "text");
     document.getElementById("password_joinr").setAttribute("type", "text");
-    document.getElementById("password_see_join").innerHTML = "Hide the password"
-    psj = true
-  }
-  else {
+    document.getElementById("password_see_join").innerHTML =
+      "Hide the password";
+    psj = true;
+  } else {
     document.getElementById("password_join").setAttribute("type", "password");
     document.getElementById("password_joinr").setAttribute("type", "password");
-    document.getElementById("password_see_join").innerHTML = "Show the password"
-    psj = false
+    document.getElementById("password_see_join").innerHTML =
+      "Show the password";
+    psj = false;
   }
-})
+});
 
 let psl = false;
 
 document.getElementById("password_see_login").addEventListener("click", () => {
   if (psl == false) {
     document.getElementById("password_login").setAttribute("type", "text");
-    document.getElementById("password_see_login").innerHTML = "Hide the password"
-    psl = true
-  }
-  else {
+    document.getElementById("password_see_login").innerHTML =
+      "Hide the password";
+    psl = true;
+  } else {
     document.getElementById("password_login").setAttribute("type", "password");
-    document.getElementById("password_see_login").innerHTML = "Show the password"
-    psl = false
+    document.getElementById("password_see_login").innerHTML =
+      "Show the password";
+    psl = false;
   }
-})
+});
